5251c3bb504d7a4830353438b9c4c424fcb10b8c
cveizaga <39276488+cveizaga@users.noreply.github.com>
2019-05-11 15:10:44 +0200

    Add files via upload
    finally I finished this fuckin thing

--- js/script.js
+++ js/script.js
@@ -18,3 +18,32 @@ function nextSlide(){
 	currentSlide = (currentSlide+1)%slides.length;
 	slides[currentSlide].className = 'slide showing';
 }
+
+//sonido
+var freak_m = document.getElementById("sonido");
+function reproducir(){
+  freak_m.play();
+}
+function pausar(){
+  freak_m.pause();
+}
+
+
+//cargar tabla de precios
+
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Menu</th><th>Descripcion</th><th>Precio por persona</th></tr>";
+  var x = xmlDoc.getElementsByTagName("menu");
+  for (i = 0; i <x.length; i++) {
+    table += "<tr><td>" +
+    x[i].getAttribute("id_menu")+
+    "</td><td>" +
+    x[i].getElementsByTagName("descripcion")[0].childNodes[0].nodeValue +
+   "</td><td>" +
+    x[i].getAttribute("precio") +
+    "</td></tr>";
+  }
+  document.getElementById("menu_se").innerHTML = table;
+}
